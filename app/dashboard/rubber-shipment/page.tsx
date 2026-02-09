cbd1b3c014047af6408762d29a19312b3e219880
mbakesamuel <purupela@gmail.com>
2026-02-09 10:14:28 +0100

    reception-updates

--- app/dashboard/rubber-shipment/page.tsx
+++ app/dashboard/rubber-shipment/page.tsx
@@ -6,14 +6,14 @@ import { getAllRubberClasses } from "@/app/actions/rubberClassActions";
 import { getAllShippingOrderDetails } from "@/app/actions/orderDetailsActions";
 import RubberShipment from "@/app/components/rubberShipment";
 import { getAllShippingOrders } from "@/app/actions/ordersActions";
-import { getAllFieldSupply } from "@/app/actions/productActions";
+import { getProduct } from "@/app/actions/productActions";
 import { getLoadingSummaryByContract } from "@/app/actions/loadingActions";
 
 
 export default async function Shipment() {
   const agents = await getAllAgents();
   const rubberclasses = await getAllRubberClasses();
-  const products = await getAllFieldSupply();
+  const products = await getProduct();
   const packingMethods = await getAllPackingMethods();
   const orderDetails = await getAllShippingOrderDetails();
   const orders = await getAllShippingOrders();
